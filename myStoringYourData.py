430119a5cfdb2ee76c20d5fc1cbc3a191e26e042
Devin Rose <drose94@gmail.com>
2018-02-19 19:53:44 -0500

    initial upload

--- myStoringYourData.py
+++ myStoringYourData.py
@@ -1,4 +1,14 @@
 import pandas as pd
-from pandas_datareader import wb
+import os
+from alpha_vantage.timeseries import TimeSeries
+from pandas_datareader import data, wb
 from datetime import datetime
-from sqlalchemy import creative_engine
+from sqlalchemy import create_engine
+
+os.chdir(r'C:\Users\drose\sqlite_databases')
+ts = TimeSeries(key='13NK6LQQTBX98JCO', output_format='pandas', indexing_type='date')
+data, meta_data = ts.get_intraday('GOOGL')
+data
+
+engine = create_engine('sqlite:///stocks.db')
+data.to_sql('GOOG', engine)
