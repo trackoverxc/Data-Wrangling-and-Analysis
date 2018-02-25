c9351a916897ce15713523bcb2c2a540865a0112
Devin Rose <drose94@gmail.com>
2018-02-25 13:40:16 -0500

    moved

---
+++ myParallelProcessing.py
@@ -0,0 +1,44 @@
+from configparser import ConfigParser
+from urllib.parse import quote_plus
+import requests
+import os
+from multiprocessing import Pool
+import logging
+
+def get_config():
+    conf = ConfigParser()
+    conf.read('prod.cfg')
+    print('conf.read success')
+    return conf
+
+def get_lat_long(config, address):
+    print('def get_lat_long(config, address')
+    qs_dict = {'address': quote_plus(address), 'key': config.get('google', 'api_key'),}
+    logging.ddebug('Requesting weather data from google for %s', address)
+    resp = requests.get('https://maps.googleapis.com/maps/api/geocode/json', params=qs_dict)
+
+    try:
+        lat, lon = resp.json().get('result')[0].get('geometry').get('location').values()
+    except KeyError:
+        raise Exception('Could not find address: %s', address)
+    return lat, lon
+
+def upcoming_forecast(args):
+    print('def upcoming_forecast(args):')
+    config, address = args
+    lat, lon = get_lat_long(config, address)
+    logging.debug('Have lat and long for %s', address)
+    resp  =requests.get('http://api.openweathermap.org/data/2.5/forecast', params={'lat':lat, 'lon':lon, 'appid':config.get('openweather', 'api_key'), 'units': 'metric'})
+    return (address, resp.json())
+
+def get_city_forecasts(addresses):
+    config = get_config()
+    pool = Pool(processes=4)
+    return pool.map(upcoming_forecast, [(config, addy) for addy in addresses])
+
+my_weather = get_city_forecasts(['Los Angeles, CA', 'New York, NY', 'Berlin, Germany'])
+
+print(type(my_weather))
+
+for item in my_weather:
+    print(item[0])
